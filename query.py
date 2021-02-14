d8f8fe07f58efd221814a580576b7ffd394c365a
Gary <71896235+Atlae@users.noreply.github.com>
2021-02-14 02:50:04 -0800

    Initialized files

--- query.py
+++ query.py
@@ -7,7 +7,9 @@ from bs4 import BeautifulSoup
 
 # puppets = list(filter(None, puppets))
 
-query = 'http://azure.nsr3n.info/card_queries/get_daemon.sh?query=%2Bregion%3Athe_east_pacific%0D%0A-deck%3As2_tep_collector&season=2&format=full&submit=submit'
+print('Running...accessing r3n\'s server')
+# query = 'http://azure.nsr3n.info/card_queries/get_daemon.sh?query=%2Bregion%3Athe_east_pacific%0D%0A-deck%3As1_tep_collector&season=1&format=full&submit=submit' # for S1
+query = 'http://azure.nsr3n.info/card_queries/get_daemon.sh?query=%2Bregion%3Athe_east_pacific%0D%0A-deck%3As2_tep_collector&season=2&format=full&submit=submit' # for S2
 reqs = requests.get(query)
 soup = BeautifulSoup(reqs.text, 'html.parser')
 
